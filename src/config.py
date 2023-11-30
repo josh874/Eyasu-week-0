58ff3ca24c81aa6dcf1e514470b8698b850ae366
Eyasu Wendwesen <eyasuwendwesen1@gmail.com>
2023-11-30 12:17:43 +0300

    First commit

--- src/config.py
+++ src/config.py
@@ -9,4 +9,16 @@ parser.add_argument('--channel', type=str, default='', help='which channel we pa
 parser.add_argument('--userfile', type=str, default='users.json', help='users profile information')
 
 cfg = parser.parse_args()
-# print(cfg)
+
+# Adjusted parameters 
+cfg.output = 'anonymized.csv'
+cfg.path = 'C:/Users/dell/Desktop/anonymized'
+cfg.channel = 'general'
+cfg.userfile = 'user_profiles.json'
+
+
+# print(cfg) 
+print(cfg.output)
+print(cfg.path)
+print(cfg.channel)
+print(cfg.userfile)
